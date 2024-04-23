24b1034592354e90048bbe2326bf5c47b0a91e92
Andherson Castro <agcl82@gmail.com>
2024-04-22 21:20:59 -0400

    Coffee Maker Updt 2

--- javascript.js
+++ javascript.js
@@ -11,6 +11,9 @@ let moccaButton = document.getElementById('mocca-btn');
 let latteButton = document.getElementById('latte-btn');
 let cappuccinoButton = document.getElementById('cappucinno-btn');
 
+//Inputs
+let paymentInput = document.getElementById('payment');
+
 let userCoffee;
 
 let coffeeMachine = {
@@ -21,13 +24,12 @@ let coffeeMachine = {
         water: 1000,
         coffee: 500,
         milk: 500,
-        chocolate: 200,
+        chocolate: 100,
         money: 0
     },
 
-    coffeTypes: {
-        
-    },
+    coffeTypes: {},
+
     //Metodos
     coffeeFactory (name, water, coffee, milk, chocolate, cost) {
         return {
@@ -35,24 +37,45 @@ let coffeeMachine = {
             water: water,
             coffee: coffee,
             milk: milk,
+            chocolate:chocolate,
             cost: cost
         }
     },
     showMenu(){
         interactiveScreen.textContent = 'Seleccione el cafe que desea en el menu de la izquierda..';
-        containerCoffee.classList.add('visible');
+        containerCoffee.classList.remove('invisible');
         buyButton.classList.add('invisible');
     },
     reset(){
         interactiveScreen.textContent = 'Bienvenido a la Coffee O-Matic 3000';
-        containerCoffee.classList.remove('visible');
+        containerCoffee.classList.add('invisible');
         buyButton.classList.remove('invisible');
-        userCoffe = '';
+        userCoffee = '';
     },
     updateScreen(userCoffee){
-        interactiveScreen.textContent = `Es cafe que usted ha seleccionado es ${userCoffee.name} y su precio es ${userCoffee.cost}$ presione continuar para avanzar.`;
+        interactiveScreen.textContent = `Es cafe que usted ha seleccionado es ${userCoffee.name} y su precio es ${userCoffee.cost}$ presione continuar para avanzar o si desea puede seleccionar otro cafe.`;
+    },
+    updateText(text){
+        interactiveScreen.textContent = text;
+    },
+    paymentCheck(payment) {
+        if (payment >= userCoffee.cost) {
+            payment -= userCoffee.cost;
+            this.inventory.money += userCoffee.cost; 
+
+            console.log('holis');
+            
+            if (payment > 0) {
+                this.updateText(`Su cambio es de ${payment}. Ya la entregaremos su cafe...`);
+            }
+            this.prepareCoffee();
+        
+            } else {
+                this.updateText('Su pago no es suficiente, por favor introduzca un monto mayor.');
+            }
     },
     selectCoffee(selection){
+        continueButton.classList.remove('invisible');
         switch (selection) {
             case 'mocca':
                 userCoffee = this.coffeTypes.moka;
@@ -70,49 +93,43 @@ let coffeeMachine = {
                 this.updateScreen(userCoffee);
                 break;
         }
+    },
+    checkInventory(){
+        payButton.classList.remove('invisible');
+        continueButton.classList.add('invisible');
+        paymentInput.classList.remove('invisible');
+
+        if (userCoffee.water > this.inventory.water) {
+            this.updateText('No hay suficiente agua para preparar su cafe. Seleccione otro cafe');
+    
+        } else if (userCoffee.coffee > this.inventory.coffee) {
+            this.updateText('No hay suficiente Cafe para preparar su cafe. Seleccione otro cafe');
+        
+        } else if (userCoffee.milk > this.inventory.milk) {
+            this.updateText('No hay suficiente Leche para preparar su cafe. Seleccione otro cafe');
+        
+        } else  if (userCoffee.chocolate > this.inventory.chocolate) {
+            this.updateText('No hay suficiente Chocolate para preparar su cafe. Seleccione otro cafe');
+        
+        } else {
+            this.updateText('Prepararemos pronto su Cafe! Introduzca el pago para continuar');
+
+        }
+    },
+    prepareCoffee () {
+        console.log(this.inventory);
+        this.inventory.water -= userCoffee.water;
+        this.inventory.milk -= userCoffee.milk;
+        this.inventory.chocolate -= userCoffee.chocolate;
+        this.inventory.coffee -= userCoffee.coffee;
+        console.log(this.inventory);
     }
 }
 
 //Creamos los Cafes
 coffeeMachine.coffeTypes.moka = coffeeMachine.coffeeFactory('Moka', 100, 50, 100, 0, 2);
-coffeeMachine.coffeTypes.cappuccino = coffeeMachine.coffeeFactory('Cappuccino', 100, 50, 100, 0, 2);
+coffeeMachine.coffeTypes.cappuccino = coffeeMachine.coffeeFactory('Cappuccino', 100, 50, 100, 100, 2);
 coffeeMachine.coffeTypes.latte = coffeeMachine.coffeeFactory('Latte', 100, 50, 100, 0, 2);
-
-
-/*const coffeeFactory = (name, water, coffee, milk, chocolate, cost) => {
-    return {
-        name: name,
-        water: water,
-        coffee: coffee,
-        milk: milk,
-        cost: cost
-    }
-}
-*/
-
-//const moka = coffeeMachine.coffeeFactory('Moka', 100, 50, 100, 0, 2);
-//const moka = coffeeFactory('Moka', 100, 50, 100, 0, 2);
-
-/*
-Tipos de Cafe
-a: Latte
-b: Moka
-c: Cappucino
-*/
-let coffeeTypes = [[100, 50, 100, 0, 1, 'Latte'], [100, 50, 100, 0, 2, 'Moka'], [100, 50, 100, 50, 2, 'Cappucino']];
-
-
-/*
-Inventario
-0: Agua
-1: Cafe
-2: Leche
-3: Chocolate
-4: Dinero o Costo
-5: Nombre
-*/
-let inventory = [1000, 500, 500, 0, 0];
-
 
 
 let switchMaquina = false;
@@ -129,115 +146,26 @@ let selectCappuccino = () => {
     coffeeMachine.selectCoffee('cappuccino');
 }
 
+let actPayment = () => {
+    coffeeMachine.paymentCheck(paymentInput.value);
+}
 
+let actCheck = () => {
+    coffeeMachine.checkInventory(userCoffee);
+}
+
+//EventListeners de los botones
 buyButton.addEventListener('click', coffeeMachine.showMenu);
 cancelButton.addEventListener('click', coffeeMachine.reset);
+payButton.addEventListener('click', actPayment);
+continueButton.addEventListener('click', actCheck);
+
 moccaButton.addEventListener('click', selectMocca);
 latteButton.addEventListener('click', selectLatte);
 cappuccinoButton.addEventListener('click', selectCappuccino);
-//continueButton.
-
-const buyCoffee = () => {
-    let userOption = prompt('Desea comprar un cafe? 1) Si - 2) No');
-    
-    if (userOption == 1) {
-        alert('Vale, ya te preparo tu cafe');
-        chooseCoffee();
-    } else if (userOption == 2) {
-        alert('La maquina se apagara.. Si desea activarla nuevamente presione F5');
-        switchMaquina = 0;
-    } else if (userOption == 3) {
-        printReport();
-    } else if (userOption == 4) {
-        rechargeMachine();
-    } else {
-        alert('Opcion no valida, seleccione otra opcion');
-        buyCoffee();
-    }
-}
 
 
-const chooseCoffee = () => {
-    let userCoffee = prompt('Que cafe desea? a) Latte - b) Moka - c) Cappucino d) Salir');
-    userCoffee = userCoffee.toUpperCase();
-    
-    if (userCoffee === 'A' || userCoffee === 'B' || userCoffee === 'C') {
-        coffeValidation(userCoffee);
-    } else if (userCoffee === 'D') {
-        alert('Usted ha seleccionado salir');
-    } else {
-        alert('Esta opcion no es valida')
-    }
-}
 
-const exitCoffeeMaker = () => {
-    alert('Hasta Luego!');
-}
-
-const coffeValidation = (userCoffee) => {
-    if (userCoffee === 'A') {
-        alert('Usted ha seleccionado Latte');
-        let coffeeSelection = coffeeTypes[0];
-        checkInventory(coffeeSelection);
-    
-    } else if (userCoffee === 'B') {
-        alert('Usted ha seleccionado Moka');
-        let coffeeSelection = coffeeTypes[1];
-        checkInventory(coffeeSelection);
-
-    } else if (userCoffee === 'C') {
-        alert('Usted ha seleccionado Cappucino');
-        let coffeeSelection = coffeeTypes[2];
-        checkInventory(coffeeSelection);
-    }
-
-}
-
-const checkInventory = (coffeeSelection) => {
-    if (coffeeSelection[0] > inventory[0]) {
-        alert('No hay suficiente agua para preparar su cafe');
-
-    } else if (coffeeSelection[1] > inventory[1]) {
-        alert('No hay suficiente Cafe para preparar su cafe');
-    
-    } else if (coffeeSelection[2] > inventory[2]) {
-        alert('No hay suficiente Leche para preparar su cafe');
-    
-    } else  if (coffeeSelection[3] > inventory[3]) {
-        alert('No hay suficiente Chocolate para preparar su cafe');
-    
-    } else {
-        alert('Prepararemos pronto su Cafe!');
-        chargeCoffee(coffeeSelection);
-    }
-}
-
-const chargeCoffee = (coffeeSelection) => {
-    let payment = prompt(`El costo de su cafe es de ${coffeeSelection[4]}, a continuacion introduzca la cantidad de dinero con la que pagara.`);
-    let coffeeCost = coffeeSelection[4]
-if (payment >= coffeeCost) {
-    payment -= coffeeCost;
-    inventory[4] += coffeeCost; 
-    
-    if (payment > 0) {
-        alert(`Su cambio es de ${payment}. Ya la entregaremos su cafe...`);
-    }
-    prepareCoffee(coffeeSelection);
-
-    } else {
-        alert('Su pago no es suficiente');
-        chargeCoffee(coffeeSelection);
-    }
-}
-
-const prepareCoffee = (coffeeSelection) => {
-    inventory[0] -= coffeeSelection[0];
-    inventory[1] -= coffeeSelection[1];
-    inventory[2] -= coffeeSelection[2];
-    inventory[3] -= coffeeSelection[3];
-    alert(`Aqui tiene su ${coffeeSelection[5]}, Gracias por su compra`);
-    
-}
 
 const rechargeMachine = () => {
     let cash = inventory[4]
