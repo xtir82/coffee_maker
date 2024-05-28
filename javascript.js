6c84fa867a6119138986eb694dc7d573097c2b70
Andherson Castro <agcl82@gmail.com>
2024-05-28 04:54:34 -0400

    Mejoras de Interfaz y Correccion de Errores

--- javascript.js
+++ javascript.js
@@ -9,11 +9,6 @@ let payButton = document.getElementById('pay-btn');
 let resetButton = document.getElementById('reset-btn');
 let coffeeBtn = document.querySelectorAll('.coffee-btn');
 
-/*
-let moccaButton = document.getElementById('mocca-btn');
-let latteButton = document.getElementById('latte-btn');
-let cappuccinoButton = document.getElementById('cappucinno-btn');
-*/
 //Inputs
 let payForm = document.getElementById('pay-form');
 let paymentInput = document.getElementById('payment');
@@ -36,16 +31,6 @@ let coffeeMachine = {
     coffeTypesArray: [],
 
     //Metodos
-    /*coffeeFactory (name, water, coffee, milk, chocolate, cost) {
-        return {
-            name: name,
-            water: water,
-            coffee: coffee,
-            milk: milk,
-            chocolate:chocolate,
-            cost: cost
-        }
-    },*/
     showMenu(){
         interactiveScreen.textContent = 'Seleccione el cafe que desea en el menu de la izquierda..';
         containerCoffee.classList.remove('invisible');
@@ -58,7 +43,6 @@ let coffeeMachine = {
         payButton.classList.add('invisible');
         continueButton.classList.add('invisible');
         payForm.classList.add('invisible');
-        //paymentInput.classList.add('invisible');
         paymentInput.value = '';
         userCoffee = '';
     },
@@ -76,13 +60,11 @@ let coffeeMachine = {
                 payButton.classList.add('invisible');
                 buyButton.classList.remove('invisible');
                 payForm.classList.add('invisible');
-                //paymentInput.classList.add('invisible');
             } else if (payment > 0) {
                 this.updateText(`Su cambio es de ${payment}. Ya la entregaremos su cafe...`);
                 payButton.classList.add('invisible');
                 buyButton.classList.remove('invisible');
                 payForm.classList.add('invisible');
-                //paymentInput.classList.add('invisible');
             }
             this.prepareCoffee();
         
@@ -96,30 +78,11 @@ let coffeeMachine = {
         userCoffee = coffeeMachine.coffeTypesArray.filter((coffee) => {return coffee.name === selection})[0]
         console.log(userCoffee);
         this.updateText(`El cafe que usted ha seleccionado es ${userCoffee.name} y su precio es ${userCoffee.cost}$ presione continuar para avanzar o si desea puede seleccionar otro cafe.`);
-
-        /*switch (selection) {
-            case 'mocca':
-                userCoffee = this.coffeTypes.moka;
-                console.log(userCoffee);
-                this.updateText(`El cafe que usted ha seleccionado es ${userCoffee.name} y su precio es ${userCoffee.cost}$ presione continuar para avanzar o si desea puede seleccionar otro cafe.`);
-                break;
-            case 'latte':
-                userCoffee = this.coffeTypes.latte;
-                console.log(userCoffee);
-                this.updateText(`El cafe que usted ha seleccionado es ${userCoffee.name} y su precio es ${userCoffee.cost}$ presione continuar para avanzar o si desea puede seleccionar otro cafe.`);
-                break;
-            case 'cappuccino':
-                userCoffee = this.coffeTypes.cappuccino;
-                console.log(userCoffee);
-                this.updateText(`El cafe que usted ha seleccionado es ${userCoffee.name} y su precio es ${userCoffee.cost}$ presione continuar para avanzar o si desea puede seleccionar otro cafe.`);
-                break;
-        }*/
     },
     checkInventory(){
         payButton.classList.remove('invisible');
         continueButton.classList.add('invisible');
         payForm.classList.remove('invisible');
-        //paymentInput.classList.remove('invisible');
 
         if (userCoffee.water > this.inventory.water) {
             this.updateText('No hay suficiente agua para preparar su cafe. Seleccione otro cafe');
@@ -143,6 +106,8 @@ let coffeeMachine = {
         }
     },
     prepareCoffee () {
+        buyButton.classList.add('invisible');
+        cancelButton.classList.add('invisible');
         console.log(this.inventory);
         this.inventory.water -= userCoffee.water;
         this.inventory.milk -= userCoffee.milk;
@@ -153,22 +118,26 @@ let coffeeMachine = {
         setTimeout( () => {Swal.fire({
             title: "Disfruta tu cafe!!",
             text: "Ya esta Listo!",
-            icon: "success",
+            imageUrl: `img/${userCoffee.name}.png`,
+            imageWidth: 300,
+            imageHeight: 300,
+            imageAlt: `${userCoffee.name} image`,
             confirmButtonText: `<i class="fa fa-thumbs-up"></i> Gracias!`
           }).then((result) => {
             if (result.isConfirmed) {
                 coffeeMachine.reset();
             }
-          })}, 5000);
+          })}, 3000);
     }
 }
-
-//Creamos los Cafes
-/*
-coffeeMachine.coffeTypes.mocca = coffeeMachine.coffeeFactory('Mocca', 100, 50, 100, 0, 2);
-coffeeMachine.coffeTypes.cappuccino = coffeeMachine.coffeeFactory('Cappuccino', 100, 50, 100, 100, 2);
-coffeeMachine.coffeTypes.latte = coffeeMachine.coffeeFactory('Latte', 100, 50, 100, 0, 2);
-*/
+/*Swal.fire({
+  title: "Sweet!",
+  text: "Modal with a custom image.",
+  imageUrl: "https://unsplash.it/400/200",
+  imageWidth: 400,
+  imageHeight: 200,
+  imageAlt: "Custom image"
+});*/
 
 
 //EventListeners de los botones (Se usan funciones anonimas para pasar los parametros a la funcion en el EventListener)
@@ -176,11 +145,7 @@ buyButton.addEventListener('click', coffeeMachine.showMenu);
 cancelButton.addEventListener('click', coffeeMachine.reset);
 payButton.addEventListener('click', () => {coffeeMachine.paymentCheck(paymentInput.value);});
 continueButton.addEventListener('click', () => {coffeeMachine.checkInventory(userCoffee);});
-/*
-moccaButton.addEventListener('click', () => {coffeeMachine.selectCoffee('mocca');});
-latteButton.addEventListener('click', () => {coffeeMachine.selectCoffee('latte');});
-cappuccinoButton.addEventListener('click', () => {coffeeMachine.selectCoffee('cappuccino');});
-*/
+
 //Implementacion del LocalStorage
 const checkLocalInv = () => {
     if (localStorage.getItem('coffeeInventory') === null) {
@@ -215,20 +180,40 @@ resetButton.addEventListener('click', () => {
 const showCoffee = (coffee) => {
     containerCoffee.innerHTML += `
     <div class="card coffee-card">
-        <img src="img/${coffee.name}.png" class="card-img-top coffe-img" alt="...">
+        <img src="img/${coffee.name}.png" class="card-img-top coffee-img" alt="...">
         <div class="card-body">
-            <h5 class="card-title">${coffee.name}</h5>
+            <h2 class="card-title">${coffee.name}</h2>
             <p class="card-text">${coffee.description}</p>
             <a id="${coffee.name}" class="btn btn-primary buy-button coffee-btn">Seleccionar</a>
         </div>
     </div>`;
-    
-    
-    //document.getElementById(coffee.name+'-btn').addEventListener('click', () =>{ console.log('holis')});
+}
 
-}   
+const getData = async() => {
+    try {
+        const response = await fetch('./assets/data.json');
+        if (response.ok) {
+            const coffeeData = await response.json();
+            coffeeData.forEach((coffee) => {
+                coffeeMachine.coffeTypesArray.push(coffee);
+                showCoffee(coffee);
+            })
+            coffeeBtn = document.querySelectorAll('.coffee-btn');
+            coffeeBtn.forEach(btn => {btn.addEventListener("click", () =>{coffeeMachine.selectCoffee(btn.id)})});
+            
+            //funcion para guardar el menu en una variable global
+        } else {throw new Error ('Error al cargar el menu, por favor recargue la pagina o comuniquese con el administrador.');}
+    } catch(error) {
+        console.error(error);
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: error
+          });;
+    }
+}
 
-
+getData()
 
 /*coffeeMachine.coffeeFactory (name, water, coffee, milk, chocolate, cost, description, picture) {
         return {
@@ -267,34 +252,7 @@ const printCoff = (event) => {
 
 
 
-const getData = async() => {
-    try {
-        const response = await fetch('./assets/data.json');
-        if (response.ok) {
-            const coffeeData = await response.json();
-            coffeeData.forEach((coffee) => {
-                coffeeMachine.coffeTypesArray.push(coffee);
-                showCoffee(coffee);
-                console.log(coffee.name);
-            })
-            coffeeBtn = document.querySelectorAll('.coffee-btn');
-            coffeeBtn.forEach(btn => {btn.addEventListener("click", () =>{coffeeMachine.selectCoffee(btn.id)})});
-            
-            //funcion para guardar el menu en una variable global
-        } else {throw new Error ('Error al cargar el menu, por favor recargue la pagina o comuniquese con el administrador.');}
-    } catch(error) {
-        console.error(error);
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: error
-          });;
-    }
-}
 
-getData()
-
-console.log(coffeeMachine.coffeTypesArray);
 
 /*
 coffeeBtn = document.getElementsByClassName('coffee-btn');
